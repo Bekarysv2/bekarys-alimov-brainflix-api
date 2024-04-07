52f9bef9ef0e4819f5b12a9d591b3beb998f53a3
Bekarys Alimov <bekarysalimov@Bekaryss-MacBook-Pro.local>
2024-04-07 20:53:35 +0100

    added cors to index.js

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const videoRoutes = require('./routes/video')
 require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+app.use(express.static('public'));
 
 app.use('/videos', videoRoutes);
 
